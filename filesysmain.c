7846751257056df81e8d0c3f51de94862764b899
Peter Parianos <gugshy123@yahoo.com>
2018-04-25 16:40:47 -0400

    added code

--- filesysmain.c
+++ filesysmain.c
@@ -8,6 +8,19 @@
 
 #include <stdio.h>
 
-int main(){
-	printf("Hello World");
+int main(int argc, char *argv[]){
+	//check arg length using passed variable named argc. The tokenization
+	// of these arguments should be stored in the 2-d array
+
+	if(argc < 2){
+		printf("Usage: %s [commands]", argv[0]);
+	}else{
+		if(argv[1] == "ls"){
+			//check for arguments and flags
+		}
+		else if(argv[1] == "cp"){
+			//check for arguments and flags
+		}
+	}
+
 }
