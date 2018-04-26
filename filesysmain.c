d6e3a103b152538491e262e8e3cd447686198a57
Peter Parianos <gugshy123@yahoo.com>
2018-04-25 20:55:51 -0400

    added handling to command line options

--- filesysmain.c
+++ filesysmain.c
@@ -7,20 +7,30 @@
 
 
 #include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]){
 	//check arg length using passed variable named argc. The tokenization
 	// of these arguments should be stored in the 2-d array
+	int opt;
+	bool longver = false;
+	bool sortbytimemod = false;
+	bool sortBySize = false;
+	while((opt = getopt (argc,argv,"lts")) != -1){
+		switch(opt){
+		case 'l': longver = true;
+		break;
+		case 't': sortbytimemod = true;
+		break;
+		case 's': sortBySize = true;
+		break;
+		default:
+			printf("Usage %: enter cp or ls first \n", argv[0]);
+		}
 
-	if(argc < 2){
-		printf("Usage: %s [commands]", argv[0]);
-	}else{
-		if(argv[1] == "ls"){
-			//check for arguments and flags
-		}
-		else if(argv[1] == "cp"){
-			//check for arguments and flags
-		}
 	}
+	int opt1 = optind;
+	printf("%d", opt1);
 
 }
