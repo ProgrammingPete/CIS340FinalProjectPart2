40cb6b74863ea0963752d83450b8dd5084e1588e
Peter Parianos <peter.parianos@yahoo.com>
2018-04-30 12:00:53 -0400

    ls works

--- filesysmain.c
+++ filesysmain.c
@@ -9,6 +9,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include	<string.h>
+
+void do_ls(char[]);
+void dostat(char *);
+void show_file_info( char *, struct stat *);
+void mode_to_letters( int , char [] );
 
 int main(int argc, char *argv[]){
 	//check arg length using passed variable named argc. The tokenization
@@ -35,16 +44,125 @@ int main(int argc, char *argv[]){
 	//int opt1 = optind;
 	//printf("%d", opt1);
 	
-	int i;
+	int i =0;
 	char str[100];
 	
-	if((strcmp(argv[1], "ls")) == 0){
-		while(argv[i][0] != '/0'){
-			if(argv[i][0] ==  134){
-				strcpy(argv[i], str);
-			}
+	if((strcmp(argv[1], "ls")) == 0){	
+			do_ls(".");
 		}
-	}else if((strcmp(argv[1], "ls")) == 0){
-		printf("hello");
+	else if((strcmp(argv[1], "cp")) == 0){
+		//printf("hello");
 	}
+}
+
+
+void do_ls( char dirname[] )
+/*
+ *	list files in directory called dirname
+ */
+{
+	DIR		*dir_ptr;		/* the directory */
+	struct dirent	*direntp;		/* each entry	 */
+
+	if ( ( dir_ptr = opendir( dirname ) ) == NULL )
+		fprintf(stderr,"ls2: cannot open %s\n", dirname);
+	else
+	{
+		while ( ( direntp = readdir( dir_ptr ) ) != NULL ) {
+			if (strcmp(direntp->d_name,".") == 0 || strcmp(direntp->d_name,"..") == 0)
+				continue;
+			dostat( direntp->d_name );
+		}
+		closedir(dir_ptr);
+	}
+}
+
+
+void dostat( char *filename )
+{
+	struct stat info;
+
+	if ( stat(filename, &info) == -1 )		/* cannot stat	 */
+		perror( filename );			/* say why	 */
+	else					/* else show info	 */
+		show_file_info( filename, &info );
+}
+
+
+void show_file_info( char *filename, struct stat *info_p )
+/*
+ * display the info about 'filename'.  The info is stored in struct at *info_p
+ */
+{
+	char	*uid_to_name(), *ctime(), *gid_to_name(), *filemode();
+	void	mode_to_letters();
+        char    modestr[11];
+
+ mode_to_letters( info_p->st_mode, modestr );
+
+	printf( "%s"    , modestr );
+	printf( "%4d "  , (int) info_p->st_nlink);
+//	printf( "%-8s " , uid_to_name(info_p->st_uid) );
+//	printf( "%-8s " , gid_to_name(info_p->st_gid) );
+	printf( "%8ld " , (long)info_p->st_size);
+	printf( "%.12s ", 4+ctime(&info_p->st_mtime));
+	printf( "%s\n"  , filename );
+
+}
+
+void mode_to_letters( int mode, char str[] )
+{
+    strcpy( str, "----------" );           /* default=no perms */
+
+    if ( S_ISDIR(mode) )  str[0] = 'd';    /* directory?       */
+    if ( S_ISCHR(mode) )  str[0] = 'c';    /* char devices     */
+    if ( S_ISBLK(mode) )  str[0] = 'b';    /* block device     */
+
+    if ( mode & S_IRUSR ) str[1] = 'r';    /* 3 bits for user  */
+    if ( mode & S_IWUSR ) str[2] = 'w';
+    if ( mode & S_IXUSR ) str[3] = 'x';
+
+    if ( mode & S_IRGRP ) str[4] = 'r';    /* 3 bits for group */
+    if ( mode & S_IWGRP ) str[5] = 'w';
+    if ( mode & S_IXGRP ) str[6] = 'x';
+
+    if ( mode & S_IROTH ) str[7] = 'r';    /* 3 bits for other */
+    if ( mode & S_IWOTH ) str[8] = 'w';
+    if ( mode & S_IXOTH ) str[9] = 'x';
+}
+
+#include	<pwd.h>
+
+char *uid_to_name( uid_t uid )
+/*
+ *	returns pointer to username associated with uid, uses getpw()
+ */
+{
+	struct	passwd *getpwuid(), *pw_ptr;
+	static  char numstr[10];
+
+	if ( ( pw_ptr = getpwuid( uid ) ) == NULL ){
+		sprintf(numstr,"%d", uid);
+		return numstr;
+	}
+	else
+		return pw_ptr->pw_name ;
+}
+
+#include	<grp.h>
+
+char *gid_to_name( gid_t gid )
+/*
+ *	returns pointer to group number gid. used getgrgid(3)
+ */
+{
+	struct group *getgrgid(), *grp_ptr;
+	static  char numstr[10];
+
+	if ( ( grp_ptr = getgrgid(gid) ) == NULL ){
+		sprintf(numstr,"%d", gid);
+		return numstr;
+	}
+	else
+		return grp_ptr->gr_name;
 }
