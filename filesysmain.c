5223116cd7f2b3076f221f52e5e8b946565a0d53
Peter Parianos <peter.parianos@yahoo.com>
2018-05-03 13:56:06 -0400

    cp is now working

--- filesysmain.c
+++ filesysmain.c
@@ -79,27 +79,31 @@ int main(int argc, char *argv[]){
         char    buf[BUFFERSIZE];
         
         /* open files	*/
+        int i;
+        for(i = 0; i < argc; i++){
+        	printf("%s\n", argv[i]);
+        }
 
-        if ( (in_fd=open(argv[1], O_RDONLY)) == -1 )
+        if ( (in_fd=open(argv[2], O_RDONLY)) == -1 )
                 oops("Cannot open ", argv[1]);
 
-        if ( (out_fd=creat( argv[2], COPYMODE)) == -1 )
+        if ( (out_fd=creat( argv[3], COPYMODE)) == -1 )
                 oops( "Cannot creat", argv[2]);
 	
 						/* copy files	*/
 
         while ( (n_chars = read(in_fd , buf, BUFFERSIZE)) > 0 )
                 if ( write( out_fd, buf, n_chars ) != n_chars )
-                        oops("Write error to ", argv[2]);
+                        oops("Write error to ", argv[3]);
         if ( n_chars == -1 )
-			oops("Read error from ", argv[1]);
+			oops("Read error from ", argv[2]);
 
 						/* close files	*/
 
         if ( close(in_fd) == -1 || close(out_fd) == -1 )
             oops("Error closing files","");
     }
-
+	return 0;
 }
 //error function
 void oops(char *s1, char *s2)
